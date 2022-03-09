4f803febe76be8714cdea8055c166d86edcdf423
iishikaydv <yishika308@gmail.com>
2022-03-09 21:17:28 +0530

    readFileSync

--- fs.js
+++ fs.js
@@ -5,3 +5,9 @@ const fs = require("fs");  // require  ->  require takes module in a string  -> 
 let res = fs.appendFileSync("f1.txt", "Hello I am F2 File.");// File ko append krta h  ->  File nhi h toh create krta h
 fs.appendFileSync("f1.txt", "You Guys are smart.");
 console.log(res);
+
+// let data = fs.readFileSync("f1.txt");  // Returns content of the path
+// console.log(typeof(data));    // data is a type of buffer(Encoding/Decoding -> ) object  
+// console.log(data);   // This buffer code is understood by just computer so we have to complete it into string
+// console.log(data + "");    // Converted buffer object into string
+let data = fs.readFileSync("f1.txt","utf-8");    // utf-8 -> Pdh k lekr aao aur esi language m odhna jo hum bhi smjh paye
