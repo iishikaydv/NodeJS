dee9d7044cc172516bfb1a313cff461ef6685ca9
iishikaydv <yishika308@gmail.com>
2022-03-10 12:09:11 +0530

    os module different functions

--- os.js
+++ os.js
@@ -3,4 +3,19 @@ const os = require("os");
 
 //returns the underlying architecture
 let mySystemArch = os.arch();  // Tells about system architecture
-console.log(mySystemArch);
+//console.log(mySystemArch);
+
+// returns information on the cpus
+let myCpuInfo = os.cpus();
+//console.log(myCpuInfo); 
+
+// returns the host name of the operating system as a string
+let hostName = os.hostname();
+//console.log(hostName); 
+
+//Returns an object containing network interfaces that have been assigned a network address
+let networkInfo = os.networkInterfaces();
+//console.log(networkInfo);
+
+// Returns the operating system as a string.
+console.log(os.release());
